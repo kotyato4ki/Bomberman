a38c18c5661137e84e170695609adeabb53dffed
vovaggri <vladimirgri0405@gmail.com>
2025-12-09 12:46:18 +0300

    first commit

---
+++ Bomberman/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bomberman
+//
+//  Created by Vladimir Grigoryev on 09.12.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
