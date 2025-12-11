6c8dc9637259d27272e577a1fdd5c71347944d12
nick <11andriianovnikita@gmail.com>
2025-12-11 14:29:22 +0300

    работает

---
+++ Bomberman/Fonts/Fonts.swift
@@ -0,0 +1,13 @@
+//
+//  Fonts.swift
+//  Bomberman
+//
+//  Created by лизо4ка курунок on 09.12.2025.
+//
+
+import UIKit
+
+enum Fonts {
+    static let pixelHeading = UIFont(name: "PixelifySans-Regular", size: 60) ?? UIFont.systemFont(ofSize: 60)
+    static let pixel27 = UIFont(name: "PixelifySans-Regular", size: 27) ?? UIFont.systemFont(ofSize: 27)
+}
